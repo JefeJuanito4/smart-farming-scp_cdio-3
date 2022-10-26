f4f708272b50150e1d1b1172929e23f766f8a7c9
TATAARC07 <115038411+TATAARC07@users.noreply.github.com>
2022-10-26 17:04:15 -0500

    Correcion de errores  y activity inventario
    se ha creado la actividad inventario y se han actualizado algunas cosas en el login

--- app/src/main/java/com/cdio/smart_farming_scp/InventarioActivity.kt
+++ app/src/main/java/com/cdio/smart_farming_scp/InventarioActivity.kt
@@ -1,19 +1,35 @@
 package com.cdio.smart_farming_scp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
-import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_auth.*
-
-class InventarioActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_inventario)
-
-        //Setup
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_inventario.*
 
 
+class InventarioActivity : AppCompatActivity(){
+
+        private lateinit var tabLayout: TabLayout
+        private lateinit var viewPager: ViewPager
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_inventario)
+
+            tabLayout = findViewById(R.id.tablayout)
+            viewPager = findViewById(R.id.viewpager)
+
+            //tabLayout.setupWithViewPager(viewPager)
+
+            val adaptador1 = Adaptador1(
+                supportFragmentManager
+            )
+            adaptador1.addFragment(PropiedadesInventario(), "Propiedades")
+            adaptador1.addFragment(DemandaInventario(), "Demanda")
+            adaptador1.addFragment(InformeInventario(), "Informe")
+            viewPager.adapter = adaptador1
+
+        }
     }
-}
