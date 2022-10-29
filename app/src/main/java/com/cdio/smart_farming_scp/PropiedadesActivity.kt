2502279e83aadb23ed2a60cdee0e76974ede6a05
nicovega17 <nicolasgarcia12342020@hotmail.com>
2022-10-29 07:07:23 -0500

    db
    base de datos casi casi

--- app/src/main/java/com/cdio/smart_farming_scp/PropiedadesActivity.kt
+++ app/src/main/java/com/cdio/smart_farming_scp/PropiedadesActivity.kt
@@ -1,43 +1,73 @@
 package com.cdio.smart_farming_scp
 
+import android.content.ContentValues.TAG
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import com.google.firebase.database.DatabaseReference
+import androidx.annotation.NonNull
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_propiedades.*
 
 class PropiedadesActivity : AppCompatActivity() {
 
-    val idp1=1
-    val idp2=2
-    val idp3=3
-    val idp4=4
-    var i=0
+    var id = 0
+
+    private val database = Firebase.database
+    private val id_propiedad = database.getReference("IDPROPIEDAD")
+    private lateinit var databaseref: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_propiedades)
 
-        val db = FirebaseFirestore.getInstance()
-
-        botonPropiedad1.setOnClickListener {
-            startActivity(Intent(this@PropiedadesActivity, ZonasP1Activity::class.java));
-        }
-
         botonmas.setOnClickListener{
-            when (i){
+            databaseref = FirebaseDatabase.getInstance().getReference("IDPROPIEDAD")
+
+            val postListener = object : ValueEventListener{
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if(dataSnapshot.exists()){
+                        var idp = dataSnapshot.value
+
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            }
+
+            when (id){
                 0 -> botonPropiedad2.visibility = View.VISIBLE
                 1 -> botonPropiedad3.visibility = View.VISIBLE
                 2 -> botonPropiedad4.visibility = View.VISIBLE
+                else -> {
+                    botonPropiedad2.visibility = View.VISIBLE
+                    botonPropiedad3.visibility = View.VISIBLE
+                    botonPropiedad4.visibility = View.VISIBLE
+                    botonmas.visibility = View.INVISIBLE
+                }
             }
-            i++
-            if(i==3){
+            if(id==3){
                 botonmas.visibility = View.INVISIBLE
             }else{
                 botonmas.visibility = View.VISIBLE
+                id++
+                if(id==3){
+                    botonmas.visibility = View.INVISIBLE
+                }
             }
+            id_propiedad.setValue(id)
+        }
+
+        botonPropiedad1.setOnClickListener {
+            startActivity(Intent(this@PropiedadesActivity, ZonasP1Activity::class.java));
         }
 
         botonPropiedad2.setOnClickListener{
