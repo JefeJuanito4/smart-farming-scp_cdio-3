3d2f5fdbeaa2f37c990749fc778c2a19a865a5f6
nicovega17 <nicolasgarcia12342020@hotmail.com>
2022-11-02 11:29:22 -0500

    db 2/3
    db 2/3

--- app/src/main/java/com/cdio/smart_farming_scp/PropiedadesActivity.kt
+++ app/src/main/java/com/cdio/smart_farming_scp/PropiedadesActivity.kt
@@ -13,78 +13,118 @@ import com.google.firebase.database.ktx.getValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_propiedades.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DatabaseReference
 
 class PropiedadesActivity : AppCompatActivity() {
 
-    var id = 0
+    var id: Int= 0
 
     private val database = Firebase.database
+    private var id_propiedad = database.getReference("USUARIO1/IDU1/IDPROPIEDADU1")
+    /**private val database = Firebase.database
     private val id_propiedad = database.getReference("IDPROPIEDAD")
-    private lateinit var databaseref: DatabaseReference
+    private lateinit var databaseref: DatabaseReference**/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_propiedades)
 
-        botonmas.setOnClickListener{
-            databaseref = FirebaseDatabase.getInstance().getReference("IDPROPIEDAD")
+        //databaseref = FirebaseDatabase.getInstance().getReference("IDPROPIEDAD")
 
-            val postListener = object : ValueEventListener{
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    if(dataSnapshot.exists()){
-                        var idp = dataSnapshot.value
+        GlobalScope.launch {
+            //var myID = id_propiedad.get().await().value
+            //myID.
+            //id = myID
 
-                    }
-                }
+            /**runOnUiThread {
+            botonmas.visibility = View.INVISIBLE
+            botonPropiedad2.visibility = View.VISIBLE
+            botonPropiedad3.visibility = View.VISIBLE
+            botonPropiedad4.visibility = View.VISIBLE
+            }**/
 
-                override fun onCancelled(error: DatabaseError) {
 
-                }
-
-            }
-
-            when (id){
-                0 -> botonPropiedad2.visibility = View.VISIBLE
-                1 -> botonPropiedad3.visibility = View.VISIBLE
-                2 -> botonPropiedad4.visibility = View.VISIBLE
-                else -> {
-                    botonPropiedad2.visibility = View.VISIBLE
-                    botonPropiedad3.visibility = View.VISIBLE
-                    botonPropiedad4.visibility = View.VISIBLE
-                    botonmas.visibility = View.INVISIBLE
-                }
-            }
-            if(id==3){
-                botonmas.visibility = View.INVISIBLE
-            }else{
+            runOnUiThread {
+                /**
                 botonmas.visibility = View.VISIBLE
-                id++
-                if(id==3){
-                    botonmas.visibility = View.INVISIBLE
+                botonPropiedad2.visibility = View.INVISIBLE
+                botonPropiedad3.visibility = View.INVISIBLE
+                botonPropiedad4.visibility = View.INVISIBLE
+
+                if (id == 1) {
+                    botonmas.visibility = View.VISIBLE
+                    botonPropiedad2.visibility = View.VISIBLE
+                    botonPropiedad3.visibility = View.INVISIBLE
+                    botonPropiedad4.visibility = View.INVISIBLE
+                } else {
+                    if (id == 2) {
+                        botonmas.visibility = View.VISIBLE
+                        botonPropiedad2.visibility = View.VISIBLE
+                        botonPropiedad3.visibility = View.VISIBLE
+                        botonPropiedad4.visibility = View.INVISIBLE
+                    } else {
+                        if (id == 3) {
+                            botonmas.visibility = View.INVISIBLE
+                            botonPropiedad2.visibility = View.VISIBLE
+                            botonPropiedad3.visibility = View.VISIBLE
+                            botonPropiedad4.visibility = View.VISIBLE
+                        }
+                    }
+                }**/
+
+                botonmas.setOnClickListener {
+                    /**if(id<3){
+                    id++
+                    }
+                    id_propiedad.setValue(id)**/
+                    when (id) {
+                        0 -> botonPropiedad2.visibility = View.VISIBLE
+                        1 -> botonPropiedad3.visibility = View.VISIBLE
+                        2 -> botonPropiedad4.visibility = View.VISIBLE
+                        else -> {
+                            botonPropiedad2.visibility = View.VISIBLE
+                            botonPropiedad3.visibility = View.VISIBLE
+                            botonPropiedad4.visibility = View.VISIBLE
+                            botonmas.visibility = View.INVISIBLE
+                        }
+                    }
+                    if (id == 3) {
+                        botonmas.visibility = View.INVISIBLE
+                    } else {
+                        botonmas.visibility = View.VISIBLE
+                        id++
+                        if (id == 3) {
+                            botonmas.visibility = View.INVISIBLE
+                        }
+                    }
+                    id_propiedad.setValue(id)
+                }
                 }
             }
-            id_propiedad.setValue(id)
-        }
 
-        botonPropiedad1.setOnClickListener {
-            startActivity(Intent(this@PropiedadesActivity, ZonasP1Activity::class.java));
-        }
+            botonPropiedad1.setOnClickListener {
+                startActivity(Intent(this@PropiedadesActivity, ZonasP1Activity::class.java));
+            }
 
-        botonPropiedad2.setOnClickListener{
-            startActivity(Intent(this@PropiedadesActivity,ZonasP2Activity::class.java));
-        }
+            botonPropiedad2.setOnClickListener {
+                startActivity(Intent(this@PropiedadesActivity, ZonasP2Activity::class.java));
+            }
 
-        botonPropiedad3.setOnClickListener{
-            startActivity(Intent(this@PropiedadesActivity,ZonasP3Activity::class.java));
-        }
+            botonPropiedad3.setOnClickListener {
+                startActivity(Intent(this@PropiedadesActivity, ZonasP3Activity::class.java));
+            }
 
-        botonPropiedad4.setOnClickListener{
-            startActivity(Intent(this@PropiedadesActivity,ZonasP4Activity::class.java));
-        }
+            botonPropiedad4.setOnClickListener {
+                startActivity(Intent(this@PropiedadesActivity, ZonasP4Activity::class.java));
+            }
 
-        //Setup
-        val bundle: Bundle? = intent.extras
-        val nombre: String? = bundle?.getString("email")
-        txtnombre.text = nombre.toString()
+            //Setup
+            val bundle: Bundle? = intent.extras
+            val nombre: String? = bundle?.getString("email")
+            txtnombre.text = nombre.toString()
         }
     }
--- app/src/main/java/com/cdio/smart_farming_scp/ZonasP3Activity.kt
+++ app/src/main/java/com/cdio/smart_farming_scp/ZonasP3Activity.kt
@@ -4,30 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_propiedades.*
 import kotlinx.android.synthetic.main.activity_zonas_p1.*
 import kotlinx.android.synthetic.main.activity_zonas_p3.*
 
 class ZonasP3Activity : AppCompatActivity() {
 
-    var i=0
+    var p3=0
+
+    //private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_zonas_p3)
+        botonZona31.visibility = View.VISIBLE
+        botonZona32.visibility = View.INVISIBLE
+        botonZona33.visibility = View.INVISIBLE
+        botonZona34.visibility = View.INVISIBLE
 
-        botonZona31.setOnClickListener {
+        /**botonZona31.setOnClickListener {
             startActivity(Intent(this@ZonasP3Activity, zonasActivity::class.java));
-        }
+        }**/
 
         botonmasP3.setOnClickListener{
-            when (i){
+            when (p3){
                 0 -> botonZona32.visibility = View.VISIBLE
                 1 -> botonZona33.visibility = View.VISIBLE
                 2 -> botonZona34.visibility = View.VISIBLE
             }
-            i++
-            if(i==3){
+            p3++
+            if(p3==3){
                 botonmasP3.visibility = View.INVISIBLE
             }else{
                 botonmasP3.visibility = View.VISIBLE
