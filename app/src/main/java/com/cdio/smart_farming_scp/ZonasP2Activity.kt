93a80d9c4037cc41213cb4377ed3832ee64baf88
nicovega17 <nicolasgarcia12342020@hotmail.com>
2022-11-02 11:29:22 -0500

    db 2/3
    db 2/3

--- app/src/main/java/com/cdio/smart_farming_scp/ZonasP2Activity.kt
+++ app/src/main/java/com/cdio/smart_farming_scp/ZonasP2Activity.kt
@@ -10,24 +10,26 @@ import kotlinx.android.synthetic.main.activity_zonas_p2.*
 
 class ZonasP2Activity : AppCompatActivity() {
 
-    var i=0
+    var p2=0
+
+    //private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_zonas_p2)
-
-        botonZona21.setOnClickListener {
-            startActivity(Intent(this@ZonasP2Activity, zonasActivity::class.java));
-        }
+        botonZona21.visibility = View.VISIBLE
+        botonZona22.visibility = View.INVISIBLE
+        botonZona23.visibility = View.INVISIBLE
+        botonZona24.visibility = View.INVISIBLE
 
         botonmasP2.setOnClickListener{
-            when (i){
+            when (p2){
                 0 -> botonZona22.visibility = View.VISIBLE
                 1 -> botonZona23.visibility = View.VISIBLE
                 2 -> botonZona24.visibility = View.VISIBLE
             }
-            i++
-            if(i==3){
+            p2++
+            if(p2==3){
                 botonmasP2.visibility = View.INVISIBLE
             }else{
                 botonmasP2.visibility = View.VISIBLE
