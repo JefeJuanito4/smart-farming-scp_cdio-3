b1617e0c4ba87255bbfa55b6195ddd13e2864527
nicovega17 <nicolasgarcia12342020@hotmail.com>
2022-11-02 12:40:11 -0500

    db
    db 3/4

--- app/src/main/java/com/cdio/smart_farming_scp/ZonasP1Activity.kt
+++ app/src/main/java/com/cdio/smart_farming_scp/ZonasP1Activity.kt
@@ -9,97 +9,42 @@ import androidx.appcompat.app.AppCompatActivity as AppCompatActivity1
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
 
 class ZonasP1Activity: AppCompatActivity1() {
 
     var p1=0
-    var idz=0
 
     private val database = Firebase.database
     private var id_zona11 = database.getReference("USUARIO1/IDU1/IDZONAU11")
-    private var id_zonaU1 = database.getReference("ID/IDBOTONZONA")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_zonas_p1)
-
-        GlobalScope.launch {
-            var myID = id_zona11.get().await().value as Long
-            p1 = myID.toInt()
-
-            runOnUiThread {
-                botonmasP1.visibility = View.VISIBLE
-                botonZona11.visibility = View.VISIBLE
-                botonZona12.visibility = View.INVISIBLE
-                botonZona13.visibility = View.INVISIBLE
-                botonZona14.visibility = View.INVISIBLE
-
-                if (p1 == 1) {
-                    botonmasP1.visibility = View.VISIBLE
-                    botonZona12.visibility = View.VISIBLE
-                    botonZona13.visibility = View.INVISIBLE
-                    botonZona14.visibility = View.INVISIBLE
-                } else {
-                    if (p1 == 2) {
-                        botonmasP1.visibility = View.VISIBLE
-                        botonZona12.visibility = View.VISIBLE
-                        botonZona13.visibility = View.VISIBLE
-                        botonZona14.visibility = View.INVISIBLE
-                    } else {
-                        if (p1 == 3) {
-                            botonmasP1.visibility = View.VISIBLE
-                            botonZona12.visibility = View.VISIBLE
-                            botonZona13.visibility = View.VISIBLE
-                            botonZona14.visibility = View.VISIBLE
-                        }
-                    }
-                }
-
-                botonmasP1.setOnClickListener{
-                    when (p1){
-                        0 -> botonZona12.visibility = View.VISIBLE
-                        1 -> botonZona13.visibility = View.VISIBLE
-                        2 -> botonZona14.visibility = View.VISIBLE
-                    }
-                    if (p1 == 3) {
-                        botonmasP1.visibility = View.INVISIBLE
-                    } else {
-                        botonmasP1.visibility = View.VISIBLE
-                        p1++
-                        if (p1== 3) {
-                            botonmasP1.visibility = View.INVISIBLE
-                        }
-                    }
-                    id_zona11.setValue(p1)
-                }
-            }
-        }
+        botonZona11.visibility = View.VISIBLE
+        botonZona12.visibility = View.INVISIBLE
+        botonZona13.visibility = View.INVISIBLE
+        botonZona14.visibility = View.INVISIBLE
 
         botonZona11.setOnClickListener {
-            idz = 1;
-            id_zonaU1.setValue(idz)
-            startActivity(Intent(this@ZonasP1Activity, ZonasActivity::class.java))
+        startActivity(Intent(this@ZonasP1Activity, ZonasActivity::class.java));
         }
 
-        botonZona12.setOnClickListener {
-            idz = 2
-            id_zonaU1.setValue(idz)
-            startActivity(Intent(this@ZonasP1Activity, ZonasActivity::class.java))
-        }
-
-        botonZona13.setOnClickListener {
-            idz = 3
-            id_zonaU1.setValue(idz)
-            startActivity(Intent(this@ZonasP1Activity, ZonasActivity::class.java))
-        }
-
-        botonZona14.setOnClickListener {
-            idz = 4
-            id_zonaU1.setValue(idz)
-            startActivity(Intent(this@ZonasP1Activity, ZonasActivity::class.java))
+        botonmasP1.setOnClickListener{
+            when (p1){
+                0 -> botonZona12.visibility = View.VISIBLE
+                1 -> botonZona13.visibility = View.VISIBLE
+                2 -> botonZona14.visibility = View.VISIBLE
+            }
+            if (p1 == 3) {
+                botonmasP1.visibility = View.INVISIBLE
+            } else {
+                botonmasP1.visibility = View.VISIBLE
+                p1++
+                if (p1== 3) {
+                    botonmasP1.visibility = View.INVISIBLE
+                }
+            }
+            id_zona11.setValue(p1)
         }
     }
 }
--- app/src/main/java/com/cdio/smart_farming_scp/ZonasP3Activity.kt
+++ app/src/main/java/com/cdio/smart_farming_scp/ZonasP3Activity.kt
@@ -20,96 +20,38 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.DatabaseReference
-import kotlinx.android.synthetic.main.activity_zonas_p2.*
 
 class ZonasP3Activity : AppCompatActivity() {
 
     var p3=0
-    var idz=0
 
     private val database = Firebase.database
     private var id_zona13 = database.getReference("USUARIO1/IDU1/IDZONAU13")
-    private var id_zonaU1 = database.getReference("ID/IDBOTONZONA")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_zonas_p3)
+        botonZona31.visibility = View.VISIBLE
+        botonZona32.visibility = View.INVISIBLE
+        botonZona33.visibility = View.INVISIBLE
+        botonZona34.visibility = View.INVISIBLE
 
-        GlobalScope.launch {
-            var myID = id_zona13.get().await().value as Long
-            p3 = myID.toInt()
-
-            runOnUiThread {
+        botonmasP3.setOnClickListener{
+            when (p3){
+                0 -> botonZona32.visibility = View.VISIBLE
+                1 -> botonZona33.visibility = View.VISIBLE
+                2 -> botonZona34.visibility = View.VISIBLE
+            }
+            if (p3 == 3) {
+                botonmasP3.visibility = View.INVISIBLE
+            } else {
                 botonmasP3.visibility = View.VISIBLE
-                botonZona31.visibility = View.VISIBLE
-                botonZona32.visibility = View.INVISIBLE
-                botonZona33.visibility = View.INVISIBLE
-                botonZona34.visibility = View.INVISIBLE
-
-                if (p3 == 1) {
-                    botonmasP3.visibility = View.VISIBLE
-                    botonZona32.visibility = View.VISIBLE
-                    botonZona33.visibility = View.INVISIBLE
-                    botonZona34.visibility = View.INVISIBLE
-                } else {
-                    if (p3 == 2) {
-                        botonmasP3.visibility = View.VISIBLE
-                        botonZona32.visibility = View.VISIBLE
-                        botonZona33.visibility = View.VISIBLE
-                        botonZona34.visibility = View.INVISIBLE
-                    } else {
-                        if (p3 == 3) {
-                            botonmasP3.visibility = View.VISIBLE
-                            botonZona32.visibility = View.VISIBLE
-                            botonZona33.visibility = View.VISIBLE
-                            botonZona34.visibility = View.VISIBLE
-                        }
-                    }
-                }
-
-                botonmasP3.setOnClickListener {
-                    when (p3) {
-                        0 -> botonZona32.visibility = View.VISIBLE
-                        1 -> botonZona33.visibility = View.VISIBLE
-                        2 -> botonZona34.visibility = View.VISIBLE
-                    }
-                    if (p3 == 3) {
-                        botonmasP3.visibility = View.INVISIBLE
-                    } else {
-                        botonmasP3.visibility = View.VISIBLE
-                        p3++
-                        if (p3 == 3) {
-                            botonmasP3.visibility = View.INVISIBLE
-                        }
-                    }
-                    id_zona13.setValue(p3)
+                p3++
+                if (p3== 3) {
+                    botonmasP3.visibility = View.INVISIBLE
                 }
             }
+            id_zona13.setValue(p3)
         }
-
-        botonZona31.setOnClickListener {
-            idz = 1
-            id_zonaU1.setValue(idz)
-            startActivity(Intent(this@ZonasP3Activity, ZonasActivity::class.java))
-        }
-
-        botonZona32.setOnClickListener {
-            idz = 2
-            id_zonaU1.setValue(idz)
-            startActivity(Intent(this@ZonasP3Activity, ZonasActivity::class.java))
-        }
-
-        botonZona33.setOnClickListener {
-            idz = 3
-            id_zonaU1.setValue(idz)
-            startActivity(Intent(this@ZonasP3Activity, ZonasActivity::class.java))
-        }
-
-        botonZona34.setOnClickListener {
-            idz = 4
-            id_zonaU1.setValue(idz)
-            startActivity(Intent(this@ZonasP3Activity, ZonasActivity::class.java))
-        }
-
     }
 }
