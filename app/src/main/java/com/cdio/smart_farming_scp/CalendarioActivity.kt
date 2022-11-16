b380051eb76410e808072dcad1040ef986313c1f
lokoi2c <charleesjobs78@gmail.com>
2022-11-15 23:01:35 -0500

    cambios calendario
    ya traigo info de la base de datos

--- app/src/main/java/com/cdio/smart_farming_scp/CalendarioActivity.kt
+++ app/src/main/java/com/cdio/smart_farming_scp/CalendarioActivity.kt
@@ -10,13 +10,24 @@ import kotlinx.android.synthetic.main.activity_auth.*
 import android.widget.CalendarView
 import android.widget.CalendarView.OnDateChangeListener
 import android.widget.TextView
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 
 class CalendarioActivity : AppCompatActivity() {
     // on below line we are creating
     // variables for text view and calendar view
+
+    private val db = Firebase.database
     lateinit var dateTV: TextView
     lateinit var calendarView: CalendarView
+    private var id_propiedad1 = db.getReference("USUARIO1/IDU1/IDPROPIEDADU1")
+    private var id_plaemb = db.getReference("USUARIO1/PROPIEDADES1/PROPIEDAD11/ZONA111/CONTCULTIVADOS111")
+    private var plaemb = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +38,14 @@ class CalendarioActivity : AppCompatActivity() {
         dateTV = findViewById(R.id.idTVDate)
         calendarView = findViewById(R.id.calendarView)
 
+
+        GlobalScope.launch {
+            var myID = id_propiedad1.get().await().value as Long
+            var plaemb = id_plaemb.get().await().value as Long
+            println("HOLAaaaaaaaaaaaaaaa ${myID}myID")
+            println(myID)
+
+        }
         // on below line we are adding set on
         // date change listener for calendar view.
         calendarView
@@ -43,5 +62,14 @@ class CalendarioActivity : AppCompatActivity() {
                     dateTV.setText(Date)
                 })
 
+        sumar4meses( )
+
+
+
+    }
+
+    private fun sumar4meses(dia: String, mes: String, annio: String){
+     var totaldias = dia.toInt() + (mes.toInt()*30) + (annio.toInt()*365)
+        var aRecol = totaldias
     }
 }
