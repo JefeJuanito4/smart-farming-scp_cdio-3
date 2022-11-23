bdc23c533cfc5e390081e21bb38261d2ac2c777b
TATAARC07 <115038411+TATAARC07@users.noreply.github.com>
2022-11-23 16:31:24 -0500

    Actualización-FaltaValoresReales
    Mi parte completa falta remplazar con valores reales

--- app/src/main/java/com/cdio/smart_farming_scp/InventarioActivity.kt
+++ app/src/main/java/com/cdio/smart_farming_scp/InventarioActivity.kt
@@ -7,29 +7,58 @@ import com.cdio.smart_farming_scp.databinding.ActivityInventarioBinding
 
 
 
-class InventarioActivity : AppCompatActivity(){
+class InventarioActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityInventarioBinding
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInventarioBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        binding.ButtonPropInv.setOnClickListener{
-            startActivity (Intent( this@InventarioActivity, PropiedadesInventario ::class.java))
-        }
 
-        binding.ButtonInfInv.setOnClickListener{
-            startActivity (Intent ( this@InventarioActivity, InformeInventario::class.java))
+        val bundle: Bundle? = intent.extras
+        val totalracCultP1: String? = bundle?.getString("totalracCultP1")
+        val totalracEmbP1: String? = bundle?.getString("totalracEmbP1")
+        val totalracCultP2: String? = bundle?.getString("totalracCultP2")
+        val totalracEmbP2: String? = bundle?.getString("totalracEmbP2")
 
-        }
+        setup(totalracCultP1 ?: "", totalracEmbP1 ?: "", totalracCultP2 ?: "", totalracEmbP2 ?: "")
 
-        binding.ButtonEstaInv.setOnClickListener{
-            startActivity(Intent ( this@InventarioActivity, EstadisticaInventario::class.java))
 
-        }
-
-        }
     }
+
+    private fun setup(totalracCultP1: String, totalracEmbP1: String, totalracCultP2: String, totalracEmbP2: String) {
+
+
+        binding.ButtonPropInv.setOnClickListener {
+            val intent1 = Intent(this@InventarioActivity, PropiedadesInventario::class.java).apply {
+                putExtra("TracimosCultivadosP1", totalracCultP1)
+                putExtra("TracimosEmbolsadosP1", totalracEmbP1)
+                putExtra("TracimosCultivadosP2", totalracCultP2)
+                putExtra("TracimosEmbolsadosP2", totalracEmbP2)
+            }
+            startActivity(intent1)
+
+        }
+            binding.ButtonInfInv.setOnClickListener {
+                val intent3 = Intent(this@InventarioActivity, InformeInventario::class.java).apply{
+                    putExtra("TracimosCultivadosP1", totalracCultP1)
+                    putExtra("TracimosCultivadosP2", totalracCultP2)
+                }
+                startActivity(intent3)
+            }
+
+            binding.ButtonEstaInv.setOnClickListener {
+                val intent2 = Intent(this, EstadisticaInventario::class.java).apply {
+                        //putExtra("TracimosCultivadosP1", totalracCultP1)
+                        //putExtra("TracimosEmbolsadosP1", totalracEmbP1)
+                        //putExtra("TracimosCultivadosP2", totalracCultP2)
+                       // putExtra("TracimosEmbolsadosP2", totalracEmbP2)
+                    }
+                startActivity(intent2)
+
+            }
+    }
+}
--- app/src/main/java/com/cdio/smart_farming_scp/PropiedadesInventario.kt
+++ app/src/main/java/com/cdio/smart_farming_scp/PropiedadesInventario.kt
@@ -1,21 +1,41 @@
 package com.cdio.smart_farming_scp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.cdio.smart_farming_scp.databinding.PropiedadesInventarioBinding
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 
 class PropiedadesInventario : AppCompatActivity() {
-
+    private val DT = Firebase.database
     private lateinit var binding: PropiedadesInventarioBinding
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = PropiedadesInventarioBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-    }
+        val bundle: Bundle? = intent.extras
+        val cantTotemb1: String? = bundle?.getString("TracimosEmbolsadosP1")
+        binding.CantEmb1.text = cantTotemb1.toString()
 
+        val cantTotcos1: String? = bundle?.getString("TracimosCultivadosP1")
+        binding.CantCos1.text = cantTotcos1.toString()
+
+        val cantTotemb2: String? = bundle?.getString("TracimosCultivadosP2")
+        binding.CantEmb2.text = cantTotemb2.toString()
+
+        val cantTotcos2: String? = bundle?.getString("TracimosCultivadosP2")
+        binding.CantCos2.text = cantTotcos2.toString()
 
     }
+}
+
